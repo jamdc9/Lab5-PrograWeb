cec5f09226e67ef82168cd2aa7d546e49aef3377
jamdc9 <jamdc9@gmail.com>
2018-10-16 21:53:18 -0600

    Lab6

--- express-lab/Index.js
+++ express-lab/Index.js
@@ -1,15 +1,162 @@
 const express = require('express');
 const app = express();
 
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/juegos';
+var db;
+mongo.connect(url, function (err, client) {
+    if (!err) {
+        console.log("We are connected");
+        db = client.db('juegos');
+    }
+});
 
 app.listen(9090);
 
 console.log('Listening on port 9090...');
 //Code
-app.use(express.json());
+app.get('/api/juegos/', function (req, res, next) {
+    db.collection('juegos').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        res.send({ juegos: result })
+    })
+});
+
+app.get('/api/juegos/:id', (req, res) => {
+    mongo.connect(url, function (err, db) {
+        const juegodb = db.db('juegos')
+        var cursor = juegosDB.collection('juegos').find({ id: parseInt(req.params.id) });
+        cursor.next(function (err, doc) {
+            if (doc) {
+                res.status(200).send(doc);
+            }
+            else {
+                res.status(404).send('ID brindado no se encontro')
+            }
+        });
+    })
+});
+
+var newID = 8
+app.post('/api/juegos', (req, res) => {
+
+    mongo.connect(url, function (err, db) {
+        const juegosdb = db.db('juegos')
+        var cursor = juegosdb.collection('juegos').find().sort({ id: -1 });
+
+        cursor.next(function (err, doc) {
+            if (doc) {
+                const juego = {
+                    id: doc.id + 1,
+                    name: req.body.name,
+                    consola: req.body.consola,
+                    comentario: req.body.comentario,
+                }
+                juegosdb.collection('juegos').insertOne(juego, function (err, result) {
+                    res.status(201).send("insertado exitosamente");
+                    db.close();
+                })
+
+            }
+        });
+    })
+
+});
+router.post('/api/juegos/', function (req, res, next) {
+    const juego = {
+        name: req.body.name,
+        consola: req.body.consola,
+        comentario: req.body.comentario,
+    }
+    db.collection('juegos').insertOne(juego, function (err, result) {
+        assert.equal(null, err);
+        if (err) return console.log(err)
+        console.log("Item inserted");
+        res.status(201).send(result);
+    });
+});
+
+router.delete('/api/juegos/:id', function (req, res, next) {
+    var id = req.params.id;
+    db.collection('juegos').deleteOne({ "_id": objectId(id) }, function (err, result) {
+        assert.equal(null, err);
+        if (err) return console.log(err)
+        console.log('Item deleted');
+        res.status(204).send(result);
+    });
+});
+
+
+router.put('/api/juegos/:id', function (req, res, next) {
+    var id = req.params.id;
+    const item = {
+        name: req.body.name,
+        consola: req.body.consola,
+        comentario: req.body.comentario,
+    }
+
+    db.collection('juegos').updateOne({ "_id": objectId(id) }, { $set: item }, function (err, result) {
+        assert.equal(null, err);
+        console.log('Item updated');
+        res.status(204).send(item);
+    });
+
+});
+
+app.put('/api/juegos/:id', (req, res) => {
+
+
+    mongo.connect(url, function (err, db) {
+        const juegosDB = db.db('juegosDB');
+        var cursor = juegosDB.collection('juegos').find({ id: parseInt(req.params.id) });
+        cursor.next(function (err, doc) {
+            if (doc) {
+                juegosDB.collection('juegos').updateOne({ id: parseInt(req.params.id) }, {
+                    $set: {
+                        name: req.body.name,
+                        consola: req.body.consola,
+                        comentario: req.body.comentario,
+                    }
+                })
+
+                res.status(204).send(" Actualizado exitosamente")
+            }
+            else {
+                res.status(404).send('ID brindado no se encontro')
+            }
+        });
+
+    });
+
+
+});
+
+app.delete('/api/juegos/:id', (req, res) => {
+    
+
+    mongo.connect(url, function (err, db) {
+        const juegosDB = db.db('juegosDB');
+        var cursor = juegosDB.collection('juegos').find({ id: parseInt(req.params.id) });
+        cursor.next(function (err, doc) {
+            if (doc) {
+                juegosDB.collection('juegos').deleteOne({ id: parseInt(req.params.id) })
+                res.status(204).send("Eliminado exitosamente")
+            }
+            else {
+                res.status(404).send('ID brindado no se encontro')
+            }
+        });
+
+    });
+
+});
+/*app.use(express.json());
 
 var lastId = 7;
-var juegos = [
+var juegos1 = [
     { id: 1, name: "Spiderman", consolas: ["PS2", "PS3", "PS4"], comentario: ["Sin jugarlo."] },
     { id: 2, name: "God of War", consolas: ["PS3", "PS4", "PSVita"], comentario: ["Muy buen juego."] },
     { id: 3, name: "Pokemon", consolas: ["Game Boy Color", "Game Boy Advance", "Nintendo Game Cube", "Nintendo Wii"], comentario: ["Buenos Juegos"] },
@@ -96,4 +243,4 @@ app.delete('/api/juegos/:id', (req, res) => {
     juegos.splice(index, 1);
     res.status(204).send(juegos);
 }
-);
+);*/
